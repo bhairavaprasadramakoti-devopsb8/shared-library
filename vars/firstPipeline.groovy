954ceacc7dbc3752818c8099b76d71da4d36d670
bhairavaprasadramakoti-devopsb8 <bhairavaprasadramakoti@gmail.com>
2025-12-05 17:19:16 +0530

    Refactor pipeline to improve addition output

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -1,47 +1,60 @@
+//@Library('com.i27academy.slb@main') _
+
 import com.i27academy.builds.Calculator
 
 def call(Map pipelineParams) {
-    Calculator calculator = new Calculator(this)
-    pipeline {
-    agent {
-        label 'java-slave'
-    }
-    environment {
-        APPLICATION_NAME = "${pipelineParams.appName}"
-    }
-    stages {
-        stage ('Addition') {
-            // actually i need to perform addition
-            script{
-                echo "**** Calling add method from src folder ****"
-                println calculator.add(2,3)
-            }
-        }
-        stage ('Build') {
-            steps {
-                echo "****** Building the ${env.APPLICATION_NAME} application ******"
-            }
-        }
-        stage ('Test') {
-            steps {
-                echo "****** Testing the ${env.APPLICATION_NAME} application ******"
-            }
-        }
-        stage ('DeploytoDev') {
-            steps {
-                echo "****** Deploying the ${env.APPLICATION_NAME} application to dev environment ******"
-            }
-        }
-        stage ('DeploytoStage') {
-            steps {
-                echo "****** Deploying the ${env.APPLICATION_NAME} application to Stage environment ******"
-            }
-        }
-        stage ('DeploytoProd') {
-            steps {
-                echo "****** Deploying the ${env.APPLICATION_NAME} application to Prod environment ******"
-            }
-        }
-    }}
-}
 
+    Calculator calculator = new Calculator(this)
+
+    pipeline {
+        agent {
+            label 'java-slave'
+        }
+
+        environment {
+            APPLICATION_NAME = "${pipelineParams.appName}"
+        }
+
+        stages {
+
+            stage('Addition') {
+                steps {
+                    script {
+                        echo "**** Calling add method from src folder ****"
+                        echo "Addition Result: ${calculator.add(2, 3)}"
+                    }
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    echo "****** Building the ${env.APPLICATION_NAME} application ******"
+                }
+            }
+
+            stage('Test') {
+                steps {
+                    echo "****** Testing the ${env.APPLICATION_NAME} application ******"
+                }
+            }
+
+            stage('DeploytoDev') {
+                steps {
+                    echo "****** Deploying the ${env.APPLICATION_NAME} application to dev environment ******"
+                }
+            }
+
+            stage('DeploytoStage') {
+                steps {
+                    echo "****** Deploying the ${env.APPLICATION_NAME} application to Stage environment ******"
+                }
+            }
+
+            stage('DeploytoProd') {
+                steps {
+                    echo "****** Deploying the ${env.APPLICATION_NAME} application to Prod environment ******"
+                }
+            }
+        }
+    }
+}
